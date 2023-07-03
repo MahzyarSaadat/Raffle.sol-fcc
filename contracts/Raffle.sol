bf61ccb22966720d6c21b8eed232ee26a3ac864f
mahziar saadat <mahzyarsaadat764@gmail.com>
2023-07-03 17:55:50 +0430

    first commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,10 @@
+//Raffle
+
+//Enter the lottery (paying some amount)
+//Pick a random number (verifiably random)
+//Chainlink Oracles -> randomness , automated execution (chinlink keepers)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Raffle {}
